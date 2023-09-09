5e1008f339d73fe9d7125d9033b248ffa469ceec
Carlor Jr Diaz <cjunior90@gmail.com>
2023-09-09 12:32:32 -0500

    Date Class developed completely

--- EmployeeOOP/Classes/Date.cs
+++ EmployeeOOP/Classes/Date.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace EmployeeOOP.Classes
 {
@@ -19,9 +17,73 @@ namespace EmployeeOOP.Classes
         //Método Constructor Sobrecargado
         public Date(int year, int month, int day)
         {
-            _year = year;
-            _month = month;
-            _day = day;
+            _year = ValidateYear(year);
+            _month = ValidateMonth(month);
+            _day = ValidateDay(day, month, year);
+        }
+
+        private int ValidateDay(int day, int month, int year)
+        {
+            //Valido si el día pertenece a un año bisiesto
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                bool isLeapYear = false;
+                IsLeapYearException(isLeapYear, year);
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+            else
+            {
+                throw new DayException(String.Format("El día {0} no es válido para el mes {1}", day, month));
+            }
+        }
+
+        private void IsLeapYearException(bool isLeapYear, int year)
+        {
+            if (!isLeapYear)
+            {
+                throw new YearException(String.Format("El año {0} no es bisiesto!", year));
+            }
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            bool isLeapYear = year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+            return isLeapYear;
+        }
+
+        private int ValidateYear(int year)
+        {
+            if (year >= 1900)
+            {
+                return year;
+            }
+            else
+            {
+                throw new YearException(String.Format("El año {0} no es válido!", year));
+            }
+        }
+
+        private int ValidateMonth(int month) //60
+        {
+            if (month >= 1 && month <= 12) //FALSO
+            {
+                return month;
+            }
+            else
+            {
+                throw new MonthException(String.Format("El mes {0} no es válido!", month));
+            }
         }
 
         public override string ToString()
--- EmployeeOOP/Program.cs
+++ EmployeeOOP/Program.cs
@@ -7,24 +7,33 @@ namespace EmployeeOOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("OOP APPLICATION");
-            Console.WriteLine("---------------");
+            try
+            {
+                Console.WriteLine("OOP APPLICATION");
+                Console.WriteLine("---------------");
 
-            //Declarar algunas variables
-            int day, month, year;
+                //Declarar algunas variables
+                int day, month, year;
 
-            Console.Write("Ingresar el día: ");
-            day = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingresar el día: ");
+                day = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Ingresar el mes: ");
-            month = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingresar el mes: ");
+                month = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Ingresar el año: ");
-            year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingresar el año: ");
+                year = Convert.ToInt32(Console.ReadLine());
 
-            //Instanciar la clase Date.cs
-            Date dateObject = new Date(year, month, day);
-            Console.WriteLine(dateObject.ToString()); //23/50/23232
+                //Instanciar la clase Date.cs
+                Date dateObject = new Date(year, month, day);
+                Console.Write("\n"); //Salto de línea
+                Console.WriteLine(dateObject.ToString()); //23/50/23232
+            }
+            catch (Exception ex)
+            {
+                Console.Write("\n");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
