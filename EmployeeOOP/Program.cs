d19cb0f7300de3ff66d305e44ae0efdbd60b14d2
Carlor Jr Diaz <cjunior90@gmail.com>
2023-09-16 11:06:37 -0500

    Object SalaryEmployee creted in Program.cs

--- EmployeeOOP/Program.cs
+++ EmployeeOOP/Program.cs
@@ -13,7 +13,9 @@ namespace EmployeeOOP
                 Console.WriteLine("---------------");
 
                 //Declarar algunas variables
-                int day, month, year;
+                int day, month, year, id;
+                string firstName, lastName;
+                decimal salary;
 
                 Console.Write("Ingresar el día: ");
                 day = Convert.ToInt32(Console.ReadLine());
@@ -29,7 +31,39 @@ namespace EmployeeOOP
                 Console.Write("\n"); //Salto de línea
                 Console.WriteLine(dateObject.ToString()); //23/50/23232
 
-                Employee employee = new Employee();
+                Console.Write("Ingrese Identifiación: ");
+                id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Ingrese nombre completo: ");
+                firstName = Console.ReadLine();
+
+                Console.Write("Ingrese apellidos: ");
+                lastName = Console.ReadLine();
+
+                Console.Write("Ingrese salario devengado: ");
+                salary = Convert.ToDecimal(Console.ReadLine());
+
+                //Esta es la forma de crear objetos a partir de una clase concreta
+                SalaryEmployee salaryEmployee = new SalaryEmployee()
+                {
+                    Id = id,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    BirthDate = dateObject,
+                    HiringDate = dateObject,
+                    IsActive = true,
+                    Salary = salary
+                };
+
+                Console.Clear();
+                Console.WriteLine(salaryEmployee);
+
+                //salaryEmployee.Id = 123;
+                //salaryEmployee.FirstName = "Juan";
+                //salaryEmployee.LastName = "Gómez";
+                //salaryEmployee.BirthDate = dateObject;
+                //salaryEmployee.HiringDate = dateObject;
+                //salaryEmployee.IsActive = true;
             }
             catch (Exception ex)
             {
